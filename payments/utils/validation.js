6086e033f70ebfd87825528306163960bebc0e71
gameflix2 <oarsenaldegames@gmail.com>
2026-04-14 02:42:34 -0300

    Delete payments directory

--- payments/utils/validation.js
+++
@@ -1,9 +0,0 @@
-export function validateEmail(email) {
-  const re = /\S+@\S+\.\S+/;
-  return re.test(email);
-}
-
-export function validatePhone(phone) {
-  const re = /^\(?\d{2}\)?\s?\d{4,5}-?\d{4}$/;
-  return re.test(phone);
-}
